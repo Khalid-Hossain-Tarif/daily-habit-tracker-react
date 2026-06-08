ae0c7302be4dbb29e69235c46f2709acf4823962
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2026-06-09 00:31:19 +0600

    text change

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold text-green-600">Hello World</h1>;
+  return <h1 className="text-3xl font-bold text-green-600"> World</h1>;
 }
