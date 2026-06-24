2622651361c17c65e7b65f1b7d4095ec1312a8f8
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2026-06-25 01:51:13 +0600

    update padding

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export default function App() {
   }, [weekOffset]);
 
   return (
-    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+    <div className="max-w-2xl mx-auto py-14 px-4 flex flex-col gap-4">
       <HabitProvider>
         <Header
           visibleDates={visibleDates}
