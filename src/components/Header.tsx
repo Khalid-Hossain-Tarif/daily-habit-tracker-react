9b046557a4cb0f3276cdb2ca8b895cc12981ebc3
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2026-06-25 00:37:54 +0600

    add current date habit done feat logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
+import { isSameDay } from "date-fns";
+import { useHabits } from "../context/useHabits";
 import { Button } from "./Button";
 
 export function Header() {
+  const { habits } = useHabits();
+  const doneToday = habits?.filter((h) =>
+    h.completions.some((c) => isSameDay(c, new Date())),
+  ).length;
+
   return (
     <header className="flex items-center justify-between">
       <div className="flex flex-col gap-2">
         <h1 className="text-3xl font-bold">Habit Tracker</h1>
-        <span className="text-sm text-zinc-400">1 / 1 done today</span>
+        <span className="text-sm text-zinc-400">
+          {doneToday} / {habits?.length} done today
+        </span>
       </div>
 
       <div className="flex flex-col gap-2 items-end">
